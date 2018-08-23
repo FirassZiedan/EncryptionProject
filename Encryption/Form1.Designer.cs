d219230db65983005e5e13c320b8b780c4120c83
Firass Ziedan <firasslive@gmail.com>
2018-08-23 04:16:09 +0300

    init commit

---
+++ Encryption/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Encryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 109);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(31, 270);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(268, 132);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open File ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Encrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(31, 161);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(112, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Rail Fence Cipher ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(31, 193);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(121, 17);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Exclusive OR (XOR)";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(31, 225);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(97, 17);
+            this.radioButton5.TabIndex = 8;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Vernam Cipher ";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(171, 177);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(128, 55);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "key";
+            this.groupBox1.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 25);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(531, 279);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 10;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(488, 71);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(365, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 431);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Encryption/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Encryption
+{
+
+    public partial class Form1 : Form
+    {
+         string extion;
+         string[] Vernam = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+         //List<string> key_char = new List<string>() ;
+         List <int> key_num = new List<int>() ;
+         char [] key;
+         List<string> mess_t  = new List<string>() ;
+         List<int>  mess_n =new List<int>();
+         List<int> key_full = new List<int>();
+         List<int> enc_n = new List<int>();
+         List<string> enc_t = new List<string>();
+        
+        
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult DR = openFileDialog1.ShowDialog();
+                if (DR == DialogResult.OK)
+                {
+                    extion = openFileDialog1.FileName.ToString();
+                }//end if
+
+                string cont = "";
+                foreach (string line in File.ReadAllLines(extion))
+                {
+                    string[] parts = line.Split();
+
+                    foreach (string part in parts)
+                    {
+                        cont += " " + part;
+                    }
+
+                }
+                textBox1.Text = cont;
+            }
+            catch
+            {
+                MessageBox.Show("Select File");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            key_num.Clear();
+            mess_t.Clear();
+            mess_n.Clear();
+            key_full.Clear();
+            enc_n.Clear();
+            // enc_t.Clear();
+            textBox2.Text = " ";
+            char[] fg = textBox1.Text.ToCharArray();
+            for (int q = 0; q < fg.Length; q++)
+            {
+                if (fg[q] != ' ')
+                {
+                    string s = fg[q].ToString();
+                    mess_t.Add(s);
+                }
+            }
+
+
+            if (radioButton2.Checked)
+            {
+
+
+                string row1 = "";
+                string row2 = "";
+                string row3 = "";
+                string r1 = "";
+                string r2 = "";
+                string r3 = "";
+                string r = "";
+                int row = mess_t.Count;
+                int row_1 = (row / 2) / 2 + 1;
+                int row_2 = row / 2;
+                int row_3 = (row / 2) / 2;
+                int c = 1;
+                int a = 0;
+                for (int i = 0; i < mess_t.Count; i++)
+                {
+                    if (c == 1)
+                    {
+                        row1 += mess_t[i] + " ";
+                        row2 += "." + " ";
+                        row3 += "." + " ";
+                        r1 += mess_t[i];
+                        c = 2;
+                    }
+
+                    else if (c == 2)
+                    {
+                        row1 += "." + " ";
+                        row2 += mess_t[i] + " ";
+                        row3 += "." + " ";
+                        r2 += mess_t[i];
+                        if (a == 1)
+                        {
+                            c = 1;
+                            a = 0;
+                        }
+                        else if (a == 0)
+                            c = 3;
+                    }
+                    else if (c == 3)
+                    {
+                        row1 += "." + " ";
+                        row2 += "." + " ";
+                        row3 += mess_t[i] + " ";
+                        r3 += mess_t[i];
+
+
+                        c = 2;
+                        a = 1;
+                    }
+
+
+
+
+                }//end for 
+                string enc_t = " ";
+                textBox2.Text = row1 + "\r\n";
+                textBox2.Text += row2 + "\r\n";
+                textBox2.Text += row3;
+                r = r1 + r2 + r3;
+                r.ToCharArray();
+                for (int i = 0; i < r.Length; i++)
+                {
+                    if (i % 5 == 0)
+                        enc_t += " " + r[i].ToString();
+                    else
+                        enc_t += r[i].ToString();
+                }
+                textBox2.Text += "\r\n\r\n";
+                textBox2.Text += enc_t;
+            }//end RB2
+            //------------------------------------------------------------------------------------------------------------------
+            if (radioButton3.Checked)
+            {
+                string en = "";
+                if (textBox3.Text != "")
+                {
+                    char[] k = textBox3.Text.ToCharArray();
+                    string kb = Convert.ToString(k[0], 2);
+                    List<string> bin = new List<string>();
+
+                    for (int i = 0; i < fg.Length; i++)
+                    {
+                        string bin1 = Convert.ToString(fg[i], 2);
+                        bin.Add(bin1);
+                        listBox1.Items.Add(bin1);
+                    }
+
+                    for (int i = 0; i < bin.Count; i++)
+                    {
+
+                        char[] s1 = bin[i].ToCharArray();
+                        char[] ks = kb.ToCharArray();
+                        /*  for (int o = 0; o < ks.Length; o++)
+                              MessageBox.Show(ks[o].ToString());*/
+                        for (int q = 0; q < s1.Length; q++)
+                        {
+                            if ((s1[q] == '1' && ks[q] == '1') || (s1[q] == '0' && ks[q] == '0'))
+                            {
+                                en += "0";
+                            }
+                            else if ((s1[q] == '1' && ks[q] == '0') || (s1[q] == '0' && ks[q] == '1'))
+                            {
+                                en += "1";
+                            }
+                        }
+                        en += " ";
+
+                    }//end for 
+
+                    textBox2.Text = en;
+                }
+                else
+                 MessageBox.Show("Enter The key!!!!");
+            }
+
+
+            //------------------------------------------------------------------------------------------------------------------
+
+
+
+
+                if (radioButton5.Checked)
+                {
+                    // key_char.Clear();
+                    key_num.Clear();
+
+                    if (textBox3.Text != "")
+                    {
+                        key = textBox3.Text.ToCharArray();
+
+                        char[] fgg = textBox1.Text.ToCharArray();
+                        for (int q = 0; q < fgg.Length; q++)
+                        {
+                            if (fg[q] != ' ')
+                            {
+                                string s = fgg[q].ToString();
+                                //-----------
+                                for (int z = 0; z < Vernam.Length; z++)
+                                {
+                                    if (s == Vernam[z].ToString())
+                                    {
+                                        mess_n.Add(z);
+                                        //  listBox2.Items.Add(s + "   " + z);
+
+                                    }//end if
+                                }
+                                //-----------
+                            }
+                        }
+
+
+                        for (int a = 0; a < key.Length; a++)
+                        {
+                            for (int y = 0; y < Vernam.Length; y++)
+                            {
+                                if (key[a].ToString() == Vernam[y])
+                                {
+                                    key_num.Add(y);
+                                }
+                            }
+                        }
+                        int y1 = 0;
+                        for (int i = 0; i < mess_n.Count; i++)
+                        {
+
+
+                            if (y1 == key.Length)
+                                y1 = 0;
+
+                            key_full.Add(key_num[y1]);
+                            y1++;
+                        }//end for 
+                        string text = "";
+                        for (int i = 0; i < mess_n.Count; i++)
+                        {
+                            int wq = (mess_n[i] + key_full[i]) % 26;
+                            enc_n.Add(wq);
+                        }//end for 
+                        for (int i = 0; i < enc_n.Count; i++)
+                        {
+                            string ss = Vernam[enc_n[i]];
+
+                            enc_t.Add(ss);
+                            text += ss;
+
+                        }
+
+
+                        textBox2.Text = text;
+                    }//end if 
+                    else
+                    {
+                        MessageBox.Show("Enter The Key ");
+                    }
+
+
+                
+
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            MessageBox.Show("Enter The Key ");
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Enter The Key One Character ");
+            groupBox1.Visible = true;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          //  string c = "hello";
+           /* byte[] arr = System.Text.Encoding.ASCII.GetBytes(c);
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                listBox1.Items.Add(arr[i]);
+            }*/
+
+
+           // string  cc = new System.Text.UTF8Encoding().GetBytes("hello"); 
+           // char  str = 'h';
+           /* string val = SByte.MinValue.ToString("x");
+            byte res = Convert.ToByte(val,16 );*/
+            string bin = Convert.ToString('A', 2);
+ 
+               MessageBox.Show(bin);
+        }//end B3
+
+       
+    }
+}
